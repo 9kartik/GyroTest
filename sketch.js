ef83c100bec0441ae373cf449702e8718e645d0c
Kartik <kartik.maurya@in.unisys.com>
2016-11-07 09:32:59 +0530

    first

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var xpos, ypos;
 
 function setup() 
 {
-  var myCanvas = createCanvas(400, 400);
+  var myCanvas = createCanvas(400, 400,400);
   xpos = 200;
   ypos = 200;
   xalpha = 0;
@@ -19,18 +19,20 @@ function draw()
   // display variables
   fill(100);
   noStroke();
-  text("alpha: " + alpha, 25, 25);
+  text("alpha: " + xalpha, 25, 25);
   text("beta: " + beta, 25, 50);
   fill(255, 0, 0);
   text("gamma: " + gamma, 25, 75);
   
   push();
   translate(200, 200);
-  rotate(radians(gamma));
-  fill(255, 0, 0);
-  rect(-40, -40, 80, 80);
-  fill(0);
-  rect(-40, 40, 80, 5);
+  strokeWeight(5);
+  stroke(color(244,0,100));
+  line(0,0,40*sin(radians(xalpha)),40*cos(radians(xalpha)));
+  stroke(color(233,233,1));
+  line(0,0,40*sin(radians(beta)),40*cos(radians(beta)));
+  stroke(color(0,233,123));
+  line(0,0,40*sin(radians(gamma)),40*cos(radians(gamma)));
   pop();	
 }
 
