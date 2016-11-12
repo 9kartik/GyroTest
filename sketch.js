960d756b01561c5bedc173761adddfb1336c5ae0
9kartik <mauryakartik@gmail.com>
2016-11-12 19:42:57 +0530

    magnitudinal

--- sketch.js
+++ sketch.js
@@ -19,13 +19,15 @@ function draw()
   // display variables
   fill(100);
   noStroke();
+  fill(color(244,0,100));
   text("alpha: " + xalpha, 25, 25);
+  fill(color(233,233,1));
   text("beta: " + beta, 25, 50);
-  fill(255, 0, 0);
+  fill(0,233,124);
   text("gamma: " + gamma, 25, 75);
   
   push();
-  translate(200, 200);
+  translate(100, 200);
   strokeWeight(5);
   stroke(color(244,0,100));
   line(0,0,40*sin(radians(xalpha)),40*cos(radians(xalpha)));
@@ -33,6 +35,14 @@ function draw()
   line(0,0,40*sin(radians(beta)),40*cos(radians(beta)));
   stroke(color(0,233,123));
   line(0,0,40*sin(radians(gamma)),40*cos(radians(gamma)));
+  pop();	
+  push();
+  translate(300, 200);
+  strokeWeight(5);
+  stroke(color(244,0,100));
+  line(0,0,map(xalpha,-180,180,-10,10),0);
+  stroke(color(233,233,1));
+  line(0,0,0,map(beta,-180,180,-10,10));
   pop();	
 }
 
